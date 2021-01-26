20ca4a3a1d908b2d37b74fd4753ccfd1879c2757
Jon Arnold <jon.arnold687@my.tccd.edu>
2021-01-25 22:05:47 -0600

    class

---
+++ classwork/ConsoleApp1/ConsoleApp1/program.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * ITSE 1430
+ * spring 2021
+ * sample
+ */
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
